056dd8259f8168a5e256873a5733dfb1648f72dd
Ибрагим Махмудов <ibrohimechanic@gmail.com>
2020-10-23 09:32:21 +0500

    solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const string = n.toString();
+  const array = string.split('');
+  const reverse = array.reverse();
+  const joinReverse = reverse.join("");
+  const parse =  parseInt(joinReverse, 10);
+  return parse;
 }
